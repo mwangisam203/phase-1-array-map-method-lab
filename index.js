97d1ef6aa07256e38afb15ba9dfd8d8880b2f743
mwangisam203 <mwangisam203@gmail.com>
2023-10-02 16:24:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() {
+  const tutorialNames = [
+    "what does the this keyword mean?",
+    "what is the constructor oo pattern?",
+    "implementing blockchain web api",
+    "the test driven development workflow",
+    "what is NaN and how can we check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?"
+  ]};
+  
